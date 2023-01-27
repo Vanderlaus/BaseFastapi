94bc014fa5927387901c6ba38f8398531a19506e
Vander Luis Lauschner <vander.lauschner@proway.treina>
2023-01-27 20:59:25 -0300

    listando substantivo e individuo

--- FST/main.py
+++ FST/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
 
 app = FastAPI()
 
@@ -7,15 +9,27 @@ async def raiz():
     return {"mensagem": "Seja bem vindo ao Moredevs2Blu"}
 
 alunos = {
-    1:"Vander",
-    2:"Lirinha",
-    3:"Thiago",
-    4:"Gisele"
+    1:{"Nome" : "Vander", "Idade" : "41", "Email" : "vanderlaus@hotmail.com"},
+    2:{"Nome" : "Thiago", "Idade" : "33", "Email" : "thiago@hotmail.com"},
+    3:{"Nome" : "João", "Idade" : "22", "Email" : "joao@hotmail.com"},
+    4:{"Nome" : "Gisele", "Idade" : "24", "Email" : "gisele@hotmail.com"}
 }
 
 @app.get('/alunos')
 async def get_alunos():
     return alunos
+
+@app.get("/alunos/{aluno_id}")
+async def get_aluno(aluno_id:int):
+    try:
+        aluno = alunos[aluno_id]
+        alunos.update({"id" : aluno_id})
+        return aluno
+
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Aluno não encontrato"
+        )
 
 if __name__ == '__main__':
     import uvicorn
