de28ca6ec1fd6360867d895f579bbb5b4934fa00
Vander Luis Lauschner <vander.lauschner@proway.treina>
2023-02-13 21:53:36 -0300

    subindo refatoração

--- FST4/main.py
+++ FST4/main.py
@@ -4,8 +4,17 @@ from core.configs import settings
 
 from api.v1.api import api_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="API MOREDEVS SQLMODEL")
 app.include_router(api_router, prefix=settings.API_STR)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     import uvicorn
